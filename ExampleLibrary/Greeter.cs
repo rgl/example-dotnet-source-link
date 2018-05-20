f788e1126f8da88c9382d1bdf5ab423097540e4e
Rui Lopes <rgl@ruilopes.com>
2018-05-20 13:49:20 +0100

    add comment to source code to test whether visual studio really downloads the expected file source

--- ExampleLibrary/Greeter.cs
+++ ExampleLibrary/Greeter.cs
@@ -4,6 +4,9 @@ namespace ExampleLibrary
 {
     public static class Greeter
     {
+        // this comment is here just to test whether visual studio will really
+        // fetch the source code from the source link document and not try to
+        // decompile it somehow.
         public static string Greet(string name)
         {
             if (string.IsNullOrEmpty(name))
